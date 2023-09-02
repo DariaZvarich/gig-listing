a25afdd2db1fdf3794c27190ad5b5f3d29db87e7
Daria Zvarich <dorimoriza@gmail.com>
2023-09-02 21:53:38 +0100

    changing props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import Gig from "./Gig";
+import Gig from "./Gig"; 
 
-const App = () => {
+function App() {
   return (
     <div className="App">
-      <h1> Gig Listings </h1>
-      <Gig />
-      <Gig />
+      <h1>Gig Listings</h1>
+      <Gig
+        bandName="The Beatles"
+        imageUrl="https://www.historic-newspapers.co.uk/wp-content/uploads/sites/5/2020/03/Beatles_ad_1965_just_the_beatles_crop.jpg"
+        description="In those years, the Beatles effectively reinvented the meaning of rock and roll as a cultural form."
+        dateTime="09.08.2013"
+        location="London"
+      />
     </div>
   );
 }
--- src/Gig.jsx
+++ src/Gig.jsx
@@ -1,15 +1,18 @@
-import "./Gig.css"
-const Gig = () => {
-    return (
-      <div className="gig">
-        <h3 className="band-name">The Beatles</h3>
-        <img src= 'https://www.historic-newspapers.co.uk/wp-content/uploads/sites/5/2020/03/Beatles_ad_1965_just_the_beatles_crop.jpg' width="300" height="200"/>
-        <p className="description"> In those years the Beatles effectively reinvented the meaning of rock and roll as a cultural form.</p>
-        <p className="date-time">09.08.2013</p>
-        <p className="location">London</p>
-      </div>
-      
-    );
-  }
-  
-  export default Gig;
+import "./Gig.css"; 
+
+function Gig(props) {
+  const { bandName, imageUrl, description, dateTime, location } = props;
+
+
+  return (
+    <div className="gig">
+      <h3 className="band-name">{bandName}</h3>
+      <img src={imageUrl} alt="Band Image" width="300" height="200" />
+      <p className="description">{description}</p>
+      <p className="date-time">{dateTime}</p>
+      <p className="location">{location}</p>
+    </div>
+  );
+}
+
+export default Gig;
